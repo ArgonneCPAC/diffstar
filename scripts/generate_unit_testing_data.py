7f0ec3f856fa03540a28dbce4f4f2c410b9ae08d
Andrew Hearin <ahearin@anl.gov>
2022-08-03 07:22:45 -0500

    Add script with testing data

---
+++ scripts/generate_unit_testing_data.py
@@ -0,0 +1,39 @@
+"""
+"""
+import argparse
+import os
+import numpy as np
+from diffstar.tests.test_stars import calc_sfh_on_default_params
+from diffstar.tests.test_stars import _get_default_mah_params
+
+
+TASSO = "/Users/aphearin/work/repositories/python/diffstar/diffstar/tests/testing_data"
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("testing_drn", help="Path to testing_data directory")
+    args = parser.parse_args()
+    testing_drn = args.testing_drn
+
+    sfh_args, sfh = calc_sfh_on_default_params()
+    lgt, dt, dmhdt, log_mah, u_ms_params, u_q_params = sfh_args
+
+    default_mah_params = _get_default_mah_params()
+
+    np.savetxt(os.path.join(testing_drn, "default_params_test_lgt.txt"), lgt)
+    np.savetxt(os.path.join(testing_drn, "default_params_test_dt.txt"), dt)
+    np.savetxt(os.path.join(testing_drn, "default_params_test_dmhdt.txt"), dmhdt)
+    np.savetxt(os.path.join(testing_drn, "default_params_test_log_mah.txt"), log_mah)
+    np.savetxt(
+        os.path.join(testing_drn, "default_params_test_u_ms_params.txt"), u_ms_params
+    )
+    np.savetxt(
+        os.path.join(testing_drn, "default_params_test_u_q_params.txt"), u_q_params
+    )
+    np.savetxt(os.path.join(testing_drn, "default_params_test_sfh.txt"), sfh)
+    np.savetxt(
+        os.path.join(testing_drn, "default_params_test_diffmah_params.txt"),
+        default_mah_params,
+    )
