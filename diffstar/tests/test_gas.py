1eb200e8806e0a7a89530a131ba1fc6008999c46
Andrew Hearin <ahearin@anl.gov>
2022-08-05 07:28:24 -0500

    Removed premature implementation of lax-based lagged gas

--- diffstar/tests/test_gas.py
+++ diffstar/tests/test_gas.py
@@ -1,17 +1,15 @@
 """
 """
-import numpy as np
 from .test_diffstar_is_frozen import calc_sfh_on_default_params
-from ..gas import _get_lagged_gas, _lax_lagged_gas
+from ..gas import _get_lagged_gas
 from ..stars import SFR_PARAM_BOUNDS
 
 
-def test_lax_lagged_gas():
+def test_lagged_gas():
     args, sfh = calc_sfh_on_default_params()
     lgt, dt, dmhdt, log_mah, u_ms_params, u_q_params = args
 
     tau_dep = 2.0
     tau_dep_max = SFR_PARAM_BOUNDS["tau_dep"][3]
-    matrix_lagged_gas = _get_lagged_gas(lgt, dt, dmhdt, tau_dep, tau_dep_max)
-    lax_lagged_gas = _lax_lagged_gas(lgt, dt, dmhdt, tau_dep, tau_dep_max)
-    assert np.allclose(lax_lagged_gas, matrix_lagged_gas, rtol=1e-4)
+    lagged_gas = _get_lagged_gas(lgt, dt, dmhdt, tau_dep, tau_dep_max)
+    assert lagged_gas.shape == dmhdt.shape
