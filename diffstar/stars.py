87d55dead1904f5d2159b793d02e4de6d3056e3d
Andrew Hearin <ahearin@anl.gov>
2022-08-09 13:53:42 -0500

    Removed duplicate definition of tw_bin_jax_kern

--- diffstar/stars.py
+++ diffstar/stars.py
@@ -8,7 +8,7 @@ from collections import OrderedDict
 import numpy as np
 from diffmah.individual_halo_assembly import _calc_halo_history
 from .quenching import quenching_function
-from .utils import _sigmoid, _inverse_sigmoid, _get_dt_array
+from .utils import _sigmoid, _inverse_sigmoid, _get_dt_array, tw_bin_jax_kern
 from .gas import _get_lagged_gas
 
 TODAY = 13.8
@@ -524,69 +524,6 @@ def _gas_conversion_kern(t_form, t_acc, dt, tau_dep):
         t_form,
     )
     return tri_kern
-
-
-@jjit
-def tw_cuml_jax_kern(x, m, h):
-    """CDF of the triweight kernel.
-
-    Parameters
-    ----------
-    x : array-like or scalar
-        The value at which to evaluate the kernel.
-    m : array-like or scalar
-        The mean of the kernel.
-    h : array-like or scalar
-        The approximate 1-sigma width of the kernel.
-
-    Returns
-    -------
-    kern_cdf : array-like or scalar
-        The value of the kernel CDF.
-
-    """
-    y = (x - m) / h
-    return lax.cond(
-        y < -3,
-        lambda x: 0.0,
-        lambda x: lax.cond(
-            x > 3,
-            lambda xx: 1.0,
-            lambda xx: (
-                -5 * xx**7 / 69984
-                + 7 * xx**5 / 2592
-                - 35 * xx**3 / 864
-                + 35 * xx / 96
-                + 1 / 2
-            ),
-            x,
-        ),
-        y,
-    )
-
-
-@jjit
-def tw_bin_jax_kern(m, h, L, H):
-    """Integrated bin weight for the triweight kernel.
-
-    Parameters
-    ----------
-    m : array-like or scalar
-        The value at which to evaluate the kernel.
-    h : array-like or scalar
-        The approximate 1-sigma width of the kernel.
-    L : array-like or scalar
-        The lower bin limit.
-    H : array-like or scalar
-        The upper bin limit.
-
-    Returns
-    -------
-    bin_prob : array-like or scalar
-        The value of the kernel integrated over the bin.
-
-    """
-    return tw_cuml_jax_kern(H, m, h) - tw_cuml_jax_kern(L, m, h)
 
 
 @jjit
