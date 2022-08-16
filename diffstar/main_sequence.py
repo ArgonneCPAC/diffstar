be4a48f75e9095b89fe0ddc6e068fc601ac16ec1
Andrew Hearin <ahearin@anl.gov>
2022-08-16 08:03:11 -0500

    Add unit testing of get_lax_ms_sfh_from_mah_kern

--- diffstar/main_sequence.py
+++ diffstar/main_sequence.py
@@ -16,13 +16,13 @@ def get_lax_ms_sfh_from_mah_kern(
     lgt0=LGT0,
     tacc_integration_min=DEFAULT_T_MIN,
     fb=FB,
-    time_array=None,
-    galpop=None,
+    tobs_loop=None,
+    galpop_loop=None,
 ):
     uniform_table = jnp.linspace(0, 1, n_steps)
 
     @jjit
-    def _lax_ms_sfh_from_mah_kern(t_form, mah_params, u_ms_params):
+    def _kern(t_form, mah_params, u_ms_params):
         ms_params = _get_bounded_sfr_params(*u_ms_params)
         tau_dep = ms_params[4]
         t_min = jnp.max(jnp.array((tacc_integration_min, t_form - tau_dep)))
@@ -30,19 +30,26 @@ def get_lax_ms_sfh_from_mah_kern(
         args = t_form, mah_params, ms_params, lgt0, fb, t_table
         return _lax_ms_sfh_scalar_kern(*args)
 
-    if time_array == "vmap":
+    kern_with_tobs_loop = _get_kern_with_tobs_loop(_kern, tobs_loop)
+    lax_ms_sfh_from_mah_kern = _get_kern_with_galpop_loop(
+        kern_with_tobs_loop, galpop_loop
+    )
+
+    return lax_ms_sfh_from_mah_kern
+
+
+def _get_kern_with_tobs_loop(kern, tobs_loop):
+    if tobs_loop == "vmap":
         _t = [0, None, None]
-        ret_func0 = jjit(vmap(_lax_ms_sfh_from_mah_kern, in_axes=_t))
-    elif time_array == "scan":
+        new_kern = jjit(vmap(kern, in_axes=_t))
+    elif tobs_loop == "scan":
 
         @jjit
-        def ret_func0(tarr, mah_params, u_ms_params):
+        def new_kern(tarr, mah_params, u_ms_params):
             @jjit
             def scan_func_time_array(carryover, el):
                 t_form = el
-                sfr_at_t_form = _lax_ms_sfh_from_mah_kern(
-                    t_form, mah_params, u_ms_params
-                )
+                sfr_at_t_form = kern(t_form, mah_params, u_ms_params)
                 carryover = sfr_at_t_form
                 accumulated = sfr_at_t_form
                 return carryover, accumulated
@@ -53,19 +60,22 @@ def get_lax_ms_sfh_from_mah_kern(
             sfh = res[1]
             return sfh
 
-    elif time_array is None:
-        ret_func0 = _lax_ms_sfh_from_mah_kern
+    elif tobs_loop is None:
+        new_kern = kern
     else:
-        msg = "Input `time_array`={0} must be either `vmap` or `scan`"
-        raise ValueError(msg.format(time_array))
+        msg = "Input `tobs_loop`={0} must be either `vmap` or `scan`"
+        raise ValueError(msg.format(tobs_loop))
+    return new_kern
 
-    if galpop == "vmap":
+
+def _get_kern_with_galpop_loop(kern, galpop_loop):
+    if galpop_loop == "vmap":
         _g = [None, 0, 0]
-        ret_func = jjit(vmap(ret_func0, in_axes=_g))
-    elif galpop == "scan":
+        new_kern = jjit(vmap(kern, in_axes=_g))
+    elif galpop_loop == "scan":
 
         @jjit
-        def ret_func(t, mah_params_galpop, ms_u_params_galpop):
+        def new_kern(t, mah_params_galpop, ms_u_params_galpop):
             n_gals, n_mah_params = mah_params_galpop.shape
             n_ms_params = ms_u_params_galpop.shape[1]
             n_params = n_mah_params + n_ms_params
@@ -78,7 +88,7 @@ def get_lax_ms_sfh_from_mah_kern(
                 params = el
                 mah_params = params[:n_mah_params]
                 u_ms_params = params[n_mah_params:]
-                sfh_galpop = ret_func0(t, mah_params, u_ms_params)
+                sfh_galpop = kern(t, mah_params, u_ms_params)
                 carryover = sfh_galpop
                 accumulated = sfh_galpop
                 return carryover, accumulated
@@ -89,10 +99,9 @@ def get_lax_ms_sfh_from_mah_kern(
             sfh_galpop = res[1]
             return sfh_galpop
 
-    elif galpop is None:
-        ret_func = ret_func0
+    elif galpop_loop is None:
+        new_kern = kern
     else:
-        msg = "Input `galpop`={0} must be either `vmap` or `scan`"
-        raise ValueError(msg.format(galpop))
-
-    return ret_func
+        msg = "Input `galpop_loop`={0} must be either `vmap` or `scan`"
+        raise ValueError(msg.format(galpop_loop))
+    return new_kern
