8836bd06a0b4efe70c83124a3adf45860a2ce4b8
Andrew Hearin <ahearin@anl.gov>
2023-08-31 10:59:17 -0500

    Add testing module of defaults.py

--- diffstar/defaults.py
+++ diffstar/defaults.py
@@ -2,15 +2,21 @@
 """
 from collections import OrderedDict
 
-DEFAULT_MS_PARAMS = OrderedDict(
+import numpy as np
+
+from .kernels.main_sequence_kernels import _get_unbounded_sfr_params
+
+DEFAULT_MS_PDICT = OrderedDict(
     lgmcrit=12.0,
     lgy_at_mcrit=-1.0,
     indx_lo=1.0,
     indx_hi=-1.0,
     tau_dep=2.0,
 )
+DEFAULT_MS_PARAMS = np.array(list(DEFAULT_MS_PDICT.values()))
+DEFAULT_U_MS_PARAMS = _get_unbounded_sfr_params(*DEFAULT_MS_PARAMS)
 
-
-DEFAULT_Q_PARAMS = OrderedDict(
+DEFAULT_U_Q_PDICT = OrderedDict(
     u_lg_qt=1.0, u_lg_qs=-0.3, u_lg_drop=-1.0, u_lg_rejuv=-0.5
 )
+DEFAULT_U_Q_PARAMS = np.array(list(DEFAULT_U_Q_PDICT.values()))
---
+++ diffstar/tests/test_defaults.py
@@ -0,0 +1,25 @@
+"""
+"""
+import numpy as np
+
+from .. import defaults
+from ..kernels.main_sequence_kernels import (
+    _get_bounded_sfr_params,
+    _get_unbounded_sfr_params,
+)
+
+
+def test_default_ms_params_are_frozen():
+    p = defaults.DEFAULT_MS_PARAMS
+    frozen_defaults = np.array((12.0, -1.0, 1.0, -1.0, 2.0))
+    assert np.allclose(p, frozen_defaults)
+
+
+def test_default_ms_params_bounded_consistent_with_unbounded():
+    p = defaults.DEFAULT_MS_PARAMS
+    u_p = defaults.DEFAULT_U_MS_PARAMS
+    p2 = _get_bounded_sfr_params(*u_p)
+    assert np.allclose(p, p2, rtol=0.01)
+
+    u_p2 = _get_unbounded_sfr_params(*p)
+    assert np.allclose(u_p, u_p2, rtol=0.01)
