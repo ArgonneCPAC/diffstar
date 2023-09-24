8051e91bbd97d29912282e8dc161eb0c756aa975
Andrew Hearin <ahearin@anl.gov>
2023-09-24 12:42:35 -0500

    Add DEFAULT_MAH_PARAMS to diffstar.defaults

--- diffstar/defaults.py
+++ diffstar/defaults.py
@@ -1,6 +1,8 @@
 """
 """
 # flake8: noqa
+from collections import OrderedDict
+
 import numpy as np
 
 TODAY = 13.8
@@ -28,3 +30,6 @@ from .kernels.quenching_kernels import (
     DEFAULT_U_Q_PARAMS,
     Q_PARAM_BOUNDS_PDICT,
 )
+
+DEFAULT_MAH_PDICT = OrderedDict(logmp=12.0, logtc=0.05, early_index=2.5, late_index=1.0)
+DEFAULT_MAH_PARAMS = np.array(list(DEFAULT_MAH_PDICT.values()))
