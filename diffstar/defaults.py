ffd56e9dd6acf46d8b2201e9b4ea29df4885b2b0
Andrew Hearin <ahearin@anl.gov>
2023-09-23 19:07:12 -0500

    Improve testing of defaults.py

--- diffstar/defaults.py
+++ diffstar/defaults.py
@@ -19,12 +19,12 @@ from .kernels.main_sequence_kernels import (
     DEFAULT_MS_PARAMS,
     DEFAULT_MS_PDICT,
     DEFAULT_U_MS_PARAMS,
-    DEFAULT_U_MS_PDICT,
     INDX_K,
+    MS_PARAM_BOUNDS_PDICT,
 )
 from .kernels.quenching_kernels import (
     DEFAULT_Q_PARAMS,
+    DEFAULT_Q_PDICT,
     DEFAULT_U_Q_PARAMS,
-    DEFAULT_U_Q_PDICT,
     Q_PARAM_BOUNDS_PDICT,
 )
