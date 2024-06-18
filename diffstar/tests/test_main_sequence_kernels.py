167080f00278bc6ae5981dadb53ac02d8f052c26
beckermr <becker.mr@gmail.com>
2024-06-18 06:59:19 -0500

    fix: please the flake8

--- diffstar/tests/test_main_sequence_kernels.py
+++ diffstar/tests/test_main_sequence_kernels.py
@@ -17,8 +17,12 @@ def test_main_sequence_kernels_lax_ms_sfh_scalar_kern_scan_vs_sum():
     t_table = jnp.linspace(T_TABLE_MIN, t_form, 20)
 
     np.testing.assert_allclose(
-        _lax_ms_sfh_scalar_kern_scan(t_form, DEFAULT_MAH_PARAMS, DEFAULT_MS_PARAMS, lgt0, FB, t_table),
-        _lax_ms_sfh_scalar_kern_sum(t_form, DEFAULT_MAH_PARAMS, DEFAULT_MS_PARAMS, lgt0, FB, t_table),
+        _lax_ms_sfh_scalar_kern_scan(
+            t_form, DEFAULT_MAH_PARAMS, DEFAULT_MS_PARAMS, lgt0, FB, t_table
+        ),
+        _lax_ms_sfh_scalar_kern_sum(
+            t_form, DEFAULT_MAH_PARAMS, DEFAULT_MS_PARAMS, lgt0, FB, t_table
+        ),
         rtol=1e-6,
         atol=1e-6,
     )
