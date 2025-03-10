6f9c813bf8fb63a448a1bfd91b5e8ce83e12a563
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-03-10 18:23:23 +0100

    Adding tng fitting sh script

---
+++ scripts/run_tng_fitter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# join error into standard out file <job_name>.o<job_id>
+#PBS -j oe
+
+# account to charge
+#PBS -A galsampler
+
+# allocate {select} nodes, each with {mpiprocs} MPI processes
+#PBS -l select=1:ncpus=20:mpiprocs=20
+
+#PBS -l walltime=04:00:00
+
+# Load software
+source ~/.bash_profile
+cd ~/source/diffmah/scripts/
+
+mpirun -n 20 python history_fitting_script_TNG.py /lcrc/project/halotools/alarcon/results/tng_diffstar_tpeak/ -istart 4 -iend 8
