9dc1c937cbd35000f9df6ac5af87ccd72f85d705
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-03-25 17:36:59 +0100

    Adding sh script

--- scripts/run_SMDPL_fitter.sh
+++ scripts/run_SMDPL_fitter.sh
@@ -15,5 +15,5 @@
 source ~/.bash_profile
 cd ~/source/diffmah/scripts/
 
-mpirun -n 20 python history_fitting_script_SMDPL_tpeak.py /lcrc/project/halotools/UniverseMachine/SMDPL/sfh_binaries_dr1_bestfit/diffstar_tpeak_fits/ -indir_diffmah /lcrc/project/halotools/UniverseMachine/SMDPL/sfh_binaries_dr1_bestfit/diffmah_tpeak_fits/ -sim_name DR1  -istart 0 -iend 10
+mpirun -n 20 python history_fitting_script_SMDPL_tpeak.py /lcrc/project/halotools/UniverseMachine/SMDPL/sfh_binaries_dr1_bestfit/diffstar_tpeak_fits/ -indir /lcrc/project/halotools/UniverseMachine/SMDPL/sfh_binaries_dr1_bestfit/a_1.000000/  -indir_diffmah /lcrc/project/halotools/UniverseMachine/SMDPL/sfh_binaries_dr1_bestfit/diffmah_tpeak_fits/ -sim_name DR1  -istart 0 -iend 10
 
