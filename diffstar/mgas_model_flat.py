e55d9a567ecf73564cbf109cba793f03baa7daa1
Andrew Hearin <ahearin@anl.gov>
2025-09-26 12:51:22 -0500

    Update mgas_model_flat.py

--- diffstar/mgas_model_flat.py
+++ diffstar/mgas_model_flat.py
@@ -63,7 +63,7 @@ def calc_mgas_singlegal(sfh_params, mah_params, tarr, lgt0=LGT0, fb=FB):
             Total remaining gas mass in units of Msun
 
     """
-    ms_params, q_params = sfh_params
+    ms_params, q_params = sfh_params[:4], sfh_params[4:]
     args = (tarr, mah_params, ms_params, q_params, lgt0, fb)
     sfh = _sfh_singlegal_kern(*args)
 
@@ -128,7 +128,7 @@ def calc_mgas_galpop(sfh_params, mah_params, tarr, lgt0=LGT0, fb=FB):
             Total remaining gas mass in units of Msun
 
     """
-    ms_params, q_params = sfh_params
+    ms_params, q_params = sfh_params[:4], sfh_params[4:]
     args = (tarr, mah_params, ms_params, q_params, lgt0, fb)
     sfh = _sfh_galpop_kern(*args)
 
