c6a687d731c6601a3537a2c7dfff69a35d49f944
Andrew Hearin <ahearin@anl.gov>
2025-10-15 15:15:23 -0500

    Update DEFAULT_DIFFSTARPOP_PARAMS to smdpl_dr1_nomerging

--- diffstar/diffstarpop/tests/test_defaults.py
+++ diffstar/diffstarpop/tests/test_defaults.py
@@ -1,27 +1,31 @@
 """"""
 
 from .. import DEFAULT_DIFFSTARPOP_PARAMS
-from ..kernels.params.params_diffstarpopfits_mgash_smdpl_dr1 import (
-    DIFFSTARPOP_FITS_SMDPL_DR1_DIFFSTARPOP_PARAMS,
+from ..kernels.params.params_diffstarpopfits_mgash import (
+    DiffstarPop_Params_Diffstarpopfits_mgash,
 )
+
+DEFAULT_MODELNAME = "smdpl_dr1_nomerging"
 
 
 def test_default_params_has_same_fields_as_universemachine_dr1():
+    params = DiffstarPop_Params_Diffstarpopfits_mgash[DEFAULT_MODELNAME]
     gen = zip(
         DEFAULT_DIFFSTARPOP_PARAMS._fields,
-        DIFFSTARPOP_FITS_SMDPL_DR1_DIFFSTARPOP_PARAMS._fields,
+        params._fields,
     )
     for default_key, um_dr1_key in gen:
         assert default_key == um_dr1_key
 
 
 def test_default_params_has_same_values_as_universemachine_dr1():
+    params = DiffstarPop_Params_Diffstarpopfits_mgash[DEFAULT_MODELNAME]
     gen = zip(
         DEFAULT_DIFFSTARPOP_PARAMS._fields,
-        DIFFSTARPOP_FITS_SMDPL_DR1_DIFFSTARPOP_PARAMS._fields,
+        params._fields,
     )
     for default_key, um_dr1_key in gen:
         assert default_key == um_dr1_key
         default_val = getattr(DEFAULT_DIFFSTARPOP_PARAMS, default_key)
-        um_dr1_val = getattr(DIFFSTARPOP_FITS_SMDPL_DR1_DIFFSTARPOP_PARAMS, um_dr1_key)
+        um_dr1_val = getattr(params, um_dr1_key)
         assert default_val == um_dr1_val, default_key
