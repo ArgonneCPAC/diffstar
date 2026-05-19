44eb9d85803a0b0fa6a30f5f0826c0e4453b4ad7
nvill <natvnr@gmail.com>
2026-05-19 14:41:44 -0500

    Add test to ensure that default values are within the bounds.

--- diffstar/diffstarpop/kernels/tests/test_defaults_mgash.py
+++ diffstar/diffstarpop/kernels/tests/test_defaults_mgash.py
@@ -5,6 +5,7 @@ import numpy as np
 from ..defaults_mgash import (
     DEFAULT_DIFFSTARPOP_PARAMS,
     DEFAULT_DIFFSTARPOP_U_PARAMS,
+    DIFFSTARPOP_PBOUNDS,
     get_bounded_diffstarpop_params,
     get_unbounded_diffstarpop_params,
 )
@@ -19,3 +20,14 @@ def test_get_bounded_diffstarpop_params():
 
     for u_p, u_p2 in zip(DEFAULT_DIFFSTARPOP_U_PARAMS, u_params):
         assert np.allclose(u_p, u_p2, rtol=5e-4)
+
+
+def test_default_values_within_bounds():
+    values = DEFAULT_DIFFSTARPOP_PARAMS._asdict()
+    bounds = DIFFSTARPOP_PBOUNDS._asdict()
+
+    assert len(values) == len(bounds)
+
+    for param_name, val in values.items():
+        low, high = bounds[param_name]
+        assert low <= val <= high
