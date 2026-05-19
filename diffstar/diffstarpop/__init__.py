c055c0d1b59f5a6e6b4df1f21f60496a9366357e
nvill <natvnr@gmail.com>
2026-05-19 14:00:31 -0500

    Change bounds namedtuple name for consistency.

--- diffstar/diffstarpop/__init__.py
+++ diffstar/diffstarpop/__init__.py
@@ -4,7 +4,7 @@
 from .defaults import (
     DEFAULT_DIFFSTARPOP_PARAMS,
     DEFAULT_DIFFSTARPOP_U_PARAMS,
-    BOUNDS_DIFFSTARPOP_PARAMS,
+    DIFFSTARPOP_PBOUNDS,
     DiffstarPopParams,
     DiffstarPopUParams,
 )
